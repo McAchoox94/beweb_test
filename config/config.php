be68c0e90f24d59ecf000a09d64f6c923afdc822
Boujjou Achraf <boujjouachraf@gmail.com>
2021-08-15 20:10:09 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,13 @@
+  <!-- =======================================================
+  * Author: Boujjou Achraf
+  * License: https://www.linkedin.com/in/achrafboujjou/
+  ======================================================== -->
+
+  <?php
+
+define('DB_HOST', 'localhost'); // host name 
+define('DB_NAME', 'beweb_test'); // database name
+define('DB_USER', 'root'); // database username
+define('DB_PASS', ''); // database password
+
+?>
